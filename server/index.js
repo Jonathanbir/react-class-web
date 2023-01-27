2a21d30e3fd4eb17be67c1483c1026ee7cfc657e
Jonathanbir <jonathan79111@gmail.com>
2023-01-28 00:05:47 +0800

    commit: build server

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+let port = 8081;
+
+//連結MongoDB
+mongoose
+  .connect("mongodb://127.0.0.1:27017/mernDB")
+  .then(() => {
+    console.log("成功連結mongoDB...");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log("伺服器正在聆聽" + port + "...");
+});
